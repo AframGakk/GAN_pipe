d911baaaadbc9e2b6590dc7d8605ff3e69a553e4
Vilhjálmur Rúnar Vilhjálmsson <vilhjalmur12@ru.is>
2019-10-11 17:03:24 +0200

    mel frequency cepstral coefficents with data normalization.
    https://app.asana.com/0/1142271705924081/1139934569804081
    
    * pulling data from correct bucket
    * splitting between features and target
    * apply MFCC to features
    * normalize mfcc features
    * label encode targets
    * undersampling 50/50 to label in question
    * push data to correct bucket

---
+++ FeatureEngineering/tests/unit/test_RecordRepo.py
@@ -0,0 +1,26 @@
+from unittest import TestCase
+from unittest.mock import patch
+import os
+from services.plots.plots import plot_audio
+
+from repositories.RecordRepo import RecordRepo
+from services.Log.Log import Log
+
+os.chdir('../../')
+log = Log()
+
+class test_ClientRepo(TestCase):
+
+    #@patch.dict('repositories.ClientRepo.ClientRepo.os.environ', env_var)
+    def setUp(self):
+        self.repo= RecordRepo()
+
+    def test_init(self):
+        self.assertTrue(self.repo)
+
+    def test_getRecords(self):
+        label = 'KICK'
+        version = 1
+        data = self.repo.getRecords(label, version)
+
+        self.assertTrue(data)
